32bdd65c706ede3b675866e1cdcc26398b0d0123
GobiShanthan <gobishanthan04@gmail.com>
2023-11-15 12:38:10 -0500

    new electron setup

---
+++ main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // Load the index.html of the React app.
+  win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(createWindow);
+
+
+// const { app, BrowserWindow } = require('electron');
+
+// function createWindow() {
+//   const win = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nodeIntegration: true
+//     }
+//   });
+
+//   // Load the React application
+//   win.loadURL('http://localhost:3000');
+// }
+
+// app.whenReady().then(createWindow);
+
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit();
+//   }
+// });
+
+// app.on('activate', () => {
+//   if (BrowserWindow.getAllWindows().length === 0) {
+//     createWindow();
+//   }
+// });
